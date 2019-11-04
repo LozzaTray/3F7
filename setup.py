df5f5063aa0d9fb2b435a9b51fdb8552eb4362d4
Lawrence Tray <ltray99@gmail.com>
2019-11-04 21:48:03 +0000

    3F7 Add .vscode to gitignore

--- setup.py
+++ setup.py
@@ -7,3 +7,19 @@ f.close()
 frequencies = dict([(key, len(list(group))) for key, group in groupby(sorted(hamlet))])
 Nin = sum([frequencies[a] for a in frequencies])
 p = dict([(a,frequencies[a]/Nin) for a in frequencies])
+
+f = [0.0]
+for a in p:
+    f.append(f[-1]+p[a])
+f.pop()
+f = dict([ (a, f[k]) for a, k in zip(p, range(len(p))) ])
+
+lo, hi = 0.0, 1.0
+n = 4
+for k in range(n):
+    a = hamlet[k]
+    lohi_range = hi - lo
+    hi = lo + lohi_range * (f[a] + p[a])
+    lo = lo + lohi_range * f[a]
+
+print('lo = {0}, hi = {1}, hi-lo = {2}'.format(lo, hi, hi - lo))
