467946aa0f75a73f142f65e592412c2a8b3a7eb5
Lawrence Tray <ltray99@gmail.com>
2019-11-05 21:38:54 +0000

    3F7 Implement arithmetic coding

--- setup.py
+++ setup.py
@@ -13,13 +13,3 @@ for a in p:
     f.append(f[-1]+p[a])
 f.pop()
 f = dict([ (a, f[k]) for a, k in zip(p, range(len(p))) ])
-
-lo, hi = 0.0, 1.0
-n = 4
-for k in range(n):
-    a = hamlet[k]
-    lohi_range = hi - lo
-    hi = lo + lohi_range * (f[a] + p[a])
-    lo = lo + lohi_range * f[a]
-
-print('lo = {0}, hi = {1}, hi-lo = {2}'.format(lo, hi, hi - lo))
